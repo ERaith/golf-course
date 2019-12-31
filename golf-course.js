c496020ce981be419b779691f3dbc6af56b8b2f9
Erik Raith <raithhtiar@gmail.com>
2019-12-31 15:52:24 -0700

    First tests

--- golf-course.js
+++ golf-course.js
@@ -4,6 +4,15 @@ class GolfCourse {
     this.difficulty = difficulty;
     this.openings = openings;
     this.features = features;
+    this.currentlyPlaying = [];
+  }
+  checkInGroup(group){ 
+    if(this.openings - group.length >=0){
+      this.openings = this.openings - group.length;
+      for(var i = 0; i <group.length; i++){
+        this.currentlyPlaying.push(group[i].name);
+      }
+    }
   }
 }
 
---
+++ golfer.js
@@ -0,0 +1,8 @@
+class Golfer {
+    constructor(golfer) {
+      this.name = golfer.name;
+      this.handicap = golfer.handicap;
+    }
+  }
+
+module.exports = Golfer;
